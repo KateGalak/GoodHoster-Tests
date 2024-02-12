8401cad1dcf3c0b7a6afdefef258cdccbc1dc7d8
zhenia <zheniaeremenko@gmail.com>
2024-02-13 00:07:49 +0200

    push new test + old tests

--- page-object/main-page.ts
+++ page-object/main-page.ts
@@ -7,7 +7,10 @@ export class MainPage {
   readonly footer: Locator;
   readonly menuButtom: Locator;
   readonly orderWebHostButton: Locator;
-  
+  readonly FirstWebHostingCard: Locator;
+  readonly SecondWebHostingCard: Locator;
+  readonly ThirdWebHostingCard: Locator;
+
   constructor(page: Page) {
     this.page = page;
     this.topBar = page.locator("xpath=//div[@id='top-bar']");
@@ -15,10 +18,21 @@ export class MainPage {
     this.footer = page.locator("xpath=//div[@id='footer-low']");
     this.menuButtom = page.locator("xpath=//div[@id='menu-bottom']");
     this.orderWebHostButton = page.locator("xpath=//a[@class='button large']")
+    this.FirstWebHostingCard = page.locator("xpath=//*[contains(text(),'CP-START')]")
+    this.SecondWebHostingCard = page.locator("xpath=//*[contains(text(),'CP-4GB')]")
+    this.ThirdWebHostingCard = page.locator("xpath=//*[contains(text(),'CP-PRO')]")
 
   }
 
   async goto() {
     await this.page.goto('https://goodhoster.net/en/');
-  }  
+  }
+
+  async checkViseability() {
+    this.goto()
+    await expect(this.FirstWebHostingCard).toHaveText('CP-START')
+    await expect(this.SecondWebHostingCard).toHaveText('CP-4GB')
+    await expect(this.ThirdWebHostingCard).toHaveText('CP-PRO')
+  }
+
 }
