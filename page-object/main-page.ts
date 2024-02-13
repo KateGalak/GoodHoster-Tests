f78838b00cea1acaef02a5cae923a46f96583b54
Vicky Naumenko <vickynaumenko@gmail.com>
2024-02-13 18:05:36 +0200

    changed tests

--- page-object/main-page.ts
+++ page-object/main-page.ts
@@ -7,6 +7,12 @@ export class MainPage {
   readonly footer: Locator;
   readonly menuButtom: Locator;
   readonly orderWebHostButton: Locator;
+  readonly registerButton: Locator;
+  readonly authorization: Locator;
+  readonly phoneIcon: Locator;
+  readonly makeMoney: Locator;
+  readonly webHostTab: Locator;
+  readonly vpsTab: Locator;
   
   constructor(page: Page) {
     this.page = page;
@@ -14,7 +20,13 @@ export class MainPage {
     this.header = page.locator("xpath=//div[@class='nav-line container hide-for-small-only clearfix']");
     this.footer = page.locator("xpath=//div[@id='footer-low']");
     this.menuButtom = page.locator("xpath=//div[@id='menu-bottom']");
-    this.orderWebHostButton = page.locator("xpath=//a[@class='button large']")
+    this.orderWebHostButton = page.locator("xpath=//a[@class='button large']");
+    this.registerButton = page.locator("xpath=//div[@class='register']");
+    this.authorization = page.locator("xpath=//a[@title='Authorization']");
+    this.phoneIcon = page.locator("xpath=//*[@class='icon-item phone']");
+    this.makeMoney = page.locator("xpath=//a[@href='/en/hosting-docs/affiliate-program-partnership/']");
+    this.webHostTab = page.locator("xpath=//*[@id='shared_hosting-label']");
+    this.vpsTab = page.locator("xpath=//*[@id='vps_hosting-label']");
 
   }
 
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,34 +1,37 @@
-import { test, expect } from '@playwright/test';
-import { MainPage } from '../page-object/main-page';
+import { test, expect } from "@playwright/test";
+import { MainPage } from "../page-object/main-page";
 
-test('Check URL main page', async ({ page }) => {
+test("Check URL main page", async ({ page }) => {
   let mainPage = new MainPage(page);
 
   await mainPage.goto();
   await expect(page.url()).toContain("https://goodhoster.net/");
 });
 
-test('Check top-bar ', async ({ page }) => {
+test("Check top-bar ", async ({ page }) => {
   let mainPage = new MainPage(page);
 
   await mainPage.goto();
   await expect(mainPage.topBar).toBeVisible();
+  await mainPage.registerButton.isVisible();
+  await mainPage.authorization.isVisible();
 });
 
-test('Footer elements', async ({ page }) => {
+test("Footer elements", async ({ page }) => {
   let mainPage = new MainPage(page);
 
   await mainPage.goto();
   await expect(mainPage.footer).toBeVisible();
   await expect(mainPage.menuButtom).toBeVisible();
+  await expect(mainPage.phoneIcon).toBeVisible();
+  await expect(mainPage.makeMoney).toContainText("Make money");
 });
 
-test('Order Web-hosting button', async ({ page }) => {
+test("click tabs web-hosting, VPS", async ({ page }) => {
   let mainPage = new MainPage(page);
 
   await mainPage.goto();
-  await expect(mainPage.orderWebHostButton).toBeVisible();
-  await expect(mainPage.orderWebHostButton).toContainText("Choose a web-hosting");
+  await mainPage.webHostTab.click();
+  await mainPage.vpsTab.click();
+    
 });
-
-
