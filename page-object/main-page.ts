362e3e14d24d6eb0d9fa4c37e3fdea124000a553
TaranD <dmytro.taran@techtellent.io>
2024-02-16 17:51:20 +0200

    smoke test dtaran

--- page-object/main-page.ts
+++ page-object/main-page.ts
@@ -3,16 +3,26 @@ import { expect, type Locator, type Page } from '@playwright/test';
 export class MainPage {
   readonly page: Page;
   readonly topBar: Locator;
+  readonly topBarRegistrationBlock: Locator;
+  readonly topBarOnlineChat: Locator;
+  readonly topBarLang: Locator;
+  readonly topBarCurrency: Locator;
   readonly header: Locator;
   readonly footer: Locator;
+  readonly footerLogo: Locator;
   readonly menuButtom: Locator;
   readonly orderWebHostButton: Locator;
   
   constructor(page: Page) {
     this.page = page;
     this.topBar = page.locator("xpath=//div[@id='top-bar']");
+    this.topBarRegistrationBlock = page.locator("xpath=//div[@id='no-entered']");
+    this.topBarOnlineChat = page.locator("xpath=//div[@id='online-chat-top-bar']");
+    this.topBarLang = page.locator("xpath=//div[@id='lang']");
+    this.topBarCurrency = page.locator("xpath=//div[@id='cur-list']");
     this.header = page.locator("xpath=//div[@class='nav-line container hide-for-small-only clearfix']");
     this.footer = page.locator("xpath=//div[@id='footer-low']");
+    this.footerLogo = page.locator("xpath=//div[@class='medium-4 hide-for-small-only large-4 columns logo-foot']");
     this.menuButtom = page.locator("xpath=//div[@id='menu-bottom']");
     this.orderWebHostButton = page.locator("xpath=//a[@class='button large']")
 
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -13,6 +13,10 @@ test('Check top-bar ', async ({ page }) => {
 
   await mainPage.goto();
   await expect(mainPage.topBar).toBeVisible();
+  await expect(mainPage.topBarRegistrationBlock).toBeVisible();
+  await expect(mainPage.topBarOnlineChat).toBeVisible();
+  await expect(mainPage.topBarLang).toBeVisible();
+  await expect(mainPage.topBarCurrency).toBeVisible();
 });
 
 test('Footer elements', async ({ page }) => {
@@ -21,6 +25,7 @@ test('Footer elements', async ({ page }) => {
   await mainPage.goto();
   await expect(mainPage.footer).toBeVisible();
   await expect(mainPage.menuButtom).toBeVisible();
+  await expect(mainPage.footerLogo).toBeVisible()
 });
 
 test('Order Web-hosting button', async ({ page }) => {
@@ -29,6 +34,22 @@ test('Order Web-hosting button', async ({ page }) => {
   await mainPage.goto();
   await expect(mainPage.orderWebHostButton).toBeVisible();
   await expect(mainPage.orderWebHostButton).toContainText("Choose a web-hosting");
-});
+  
 
-
+  async function checkIfTextIsPresent(page, textsArray) {
+    const pageText = await page.textContent('body'); 
+  
+    for (const text of textsArray) {
+      if (pageText.includes(text)) {
+        return true; 
+      }
+    }
+  
+    return false; 
+  }
+    const textsToCheck = ['Choose a web-hosting','Замовити веб-хостинг','Заказать веб-хостинг'];
+    const isTextPresent = await checkIfTextIsPresent(page, textsToCheck);
+  
+    expect(isTextPresent).toBeTruthy();
+  });
+;
