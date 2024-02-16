d8762166f436fc409b2df249711ffb06e874c6e8
julia7st <juliastepanova10@gmail.com>
2024-02-16 15:51:49 +0100

    Changed some tests and added a new one

--- page-object/main-page.ts
+++ page-object/main-page.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator, type Page } from '@playwright/test';
+import { expect, type Locator, type Page } from "@playwright/test";
 
 export class MainPage {
   readonly page: Page;
@@ -6,19 +6,26 @@ export class MainPage {
   readonly header: Locator;
   readonly footer: Locator;
   readonly menuButtom: Locator;
-  readonly orderWebHostButton: Locator;
-  
+  readonly chooseWebHostButton: Locator;
+  readonly VPSTab: Locator;
+  readonly WebHostingTab: Locator;
+  readonly VMPrice: Locator;
+  readonly CPPrice: Locator;
+
   constructor(page: Page) {
     this.page = page;
     this.topBar = page.locator("xpath=//div[@id='top-bar']");
     this.header = page.locator("xpath=//div[@class='nav-line container hide-for-small-only clearfix']");
     this.footer = page.locator("xpath=//div[@id='footer-low']");
     this.menuButtom = page.locator("xpath=//div[@id='menu-bottom']");
-    this.orderWebHostButton = page.locator("xpath=//a[@class='button large']")
-
+    this.chooseWebHostButton = page.locator("xpath=//a[@class='button large']");
+    this.VPSTab = page.locator("xpath=//a[@id='vps_hosting-label']");
+    this.WebHostingTab = page.locator("xpath=//a[@id='shared_hosting-label']");
+    this.VMPrice = page.locator("xpath=//div[text()[normalize-space()='VM-20G']]");
+    this.CPPrice = page.locator("xpath=//div[text()='CP-START']");
   }
 
   async goto() {
-    await this.page.goto('https://goodhoster.net/en/');
-  }  
+    await this.page.goto("https://goodhoster.net/en/");
+  }
 }
