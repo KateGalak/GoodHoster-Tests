514fb973ad1e6de4c1845e91fef418320878f845
Evelina Kutalo <kutaloea@gmail.com>
2024-03-05 18:30:02 +0200

    added new test and beforeEach

--- page-object/main-page.ts
+++ page-object/main-page.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator, type Page } from '@playwright/test';
+import { expect, type Locator, type Page } from "@playwright/test";
 
 export class MainPage {
   readonly page: Page;
@@ -12,23 +12,36 @@ export class MainPage {
   readonly footerLogo: Locator;
   readonly menuButtom: Locator;
   readonly orderWebHostButton: Locator;
-  
+  readonly vpsTab: Locator;
+  readonly vpsHostingTabs: Locator;
+
   constructor(page: Page) {
     this.page = page;
     this.topBar = page.locator("xpath=//div[@id='top-bar']");
-    this.topBarRegistrationBlock = page.locator("xpath=//div[@id='no-entered']");
-    this.topBarOnlineChat = page.locator("xpath=//div[@id='online-chat-top-bar']");
+    this.topBarRegistrationBlock = page.locator(
+      "xpath=//div[@id='no-entered']"
+    );
+    this.topBarOnlineChat = page.locator(
+      "xpath=//div[@id='online-chat-top-bar']"
+    );
     this.topBarLang = page.locator("xpath=//div[@id='lang']");
     this.topBarCurrency = page.locator("xpath=//div[@id='cur-list']");
-    this.header = page.locator("xpath=//div[@class='nav-line container hide-for-small-only clearfix']");
+    this.header = page.locator(
+      "xpath=//div[@class='nav-line container hide-for-small-only clearfix']"
+    );
     this.footer = page.locator("xpath=//div[@id='footer-low']");
-    this.footerLogo = page.locator("xpath=//div[@class='medium-4 hide-for-small-only large-4 columns logo-foot']");
+    this.footerLogo = page.locator(
+      "xpath=//div[@class='medium-4 hide-for-small-only large-4 columns logo-foot']"
+    );
     this.menuButtom = page.locator("xpath=//div[@id='menu-bottom']");
-    this.orderWebHostButton = page.locator("xpath=//a[@class='button large']")
-
+    this.orderWebHostButton = page.locator("xpath=//a[@class='button large']");
+    this.vpsTab = page.locator("xpath=//a[@id='vps_hosting-label']");
+    this.vpsHostingTabs = page.locator(
+      "xpath=//div[@class='hosting-tabs-wrapper']"
+    );
   }
 
   async goto() {
-    await this.page.goto('https://goodhoster.net/en/');
-  }  
+    await this.page.goto("https://goodhoster.net/en/");
+  }
 }
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,55 +1,63 @@
-import { test, expect } from '@playwright/test';
-import { MainPage } from '../page-object/main-page';
+import { test, expect } from "@playwright/test";
+import { MainPage } from "../page-object/main-page";
 
-test('Check URL main page', async ({ page }) => {
-  let mainPage = new MainPage(page);
+test.describe("GoodHoster Tests", () => {
+  let mainPage: MainPage;
 
-  await mainPage.goto();
-  await expect(page.url()).toContain("https://goodhoster.net/");
-});
+  test.beforeEach(async ({ page }) => {
+    mainPage = new MainPage(page);
+    await mainPage.goto();
+  });
 
-test('Check top-bar ', async ({ page }) => {
-  let mainPage = new MainPage(page);
+  test("Check URL main page", async ({ page }) => {
+    await expect(page.url()).toContain("https://goodhoster.net/");
+  });
 
-  await mainPage.goto();
-  await expect(mainPage.topBar).toBeVisible();
-  await expect(mainPage.topBarRegistrationBlock).toBeVisible();
-  await expect(mainPage.topBarOnlineChat).toBeVisible();
-  await expect(mainPage.topBarLang).toBeVisible();
-  await expect(mainPage.topBarCurrency).toBeVisible();
-});
+  test("Check top-bar ", async ({ page }) => {
+    await expect(mainPage.topBar).toBeVisible();
+    await expect(mainPage.topBarRegistrationBlock).toBeVisible();
+    await expect(mainPage.topBarOnlineChat).toBeVisible();
+    await expect(mainPage.topBarLang).toBeVisible();
+    await expect(mainPage.topBarCurrency).toBeVisible();
+  });
 
-test('Footer elements', async ({ page }) => {
-  let mainPage = new MainPage(page);
+  test("Footer elements", async ({ page }) => {
+    await expect(mainPage.footer).toBeVisible();
+    await expect(mainPage.menuButtom).toBeVisible();
+    await expect(mainPage.footerLogo).toBeVisible();
+  });
 
-  await mainPage.goto();
-  await expect(mainPage.footer).toBeVisible();
-  await expect(mainPage.menuButtom).toBeVisible();
-  await expect(mainPage.footerLogo).toBeVisible()
-});
+  test("VPS hosting tabs are present after clicking on VPS tab", async ({
+    page,
+  }) => {
+    await mainPage.vpsTab.click();
+    // await expect(mainPage.vpsHostingTabs).toBeVisible();
+  });
 
-test('Order Web-hosting button', async ({ page }) => {
-  let mainPage = new MainPage(page);
+  test("Order Web-hosting button", async ({ page }) => {
+    await expect(mainPage.orderWebHostButton).toBeVisible();
+    await expect(mainPage.orderWebHostButton).toContainText(
+      "Choose a web-hosting"
+    );
 
-  await mainPage.goto();
-  await expect(mainPage.orderWebHostButton).toBeVisible();
-  await expect(mainPage.orderWebHostButton).toContainText("Choose a web-hosting");
-  
+    async function checkIfTextIsPresent(page, textsArray) {
+      const pageText = await page.textContent("body");
 
-  async function checkIfTextIsPresent(page, textsArray) {
-    const pageText = await page.textContent('body'); 
-  
-    for (const text of textsArray) {
-      if (pageText.includes(text)) {
-        return true; 
+      for (const text of textsArray) {
+        if (pageText.includes(text)) {
+          return true;
+        }
       }
+
+      return false;
     }
-  
-    return false; 
-  }
-    const textsToCheck = ['Choose a web-hosting','Замовити веб-хостинг','Заказать веб-хостинг'];
+    const textsToCheck = [
+      "Choose a web-hosting",
+      "Замовити веб-хостинг",
+      "Заказать веб-хостинг",
+    ];
     const isTextPresent = await checkIfTextIsPresent(page, textsToCheck);
-  
+
     expect(isTextPresent).toBeTruthy();
   });
-;
+});
